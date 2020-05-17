76795d263b6e1f4e90932d11c3ca16edc0c61595
nickk098 <34365694+nickk098@users.noreply.github.com>
2020-05-17 17:53:23 +1000

    Partial save game functionality

--- Player.h
+++ Player.h
@@ -1,23 +1,39 @@
 #ifndef AZUL_PLAYER
 #define AZUL_PLAYER
 
-#include "Types.h"
+#include "LinkedList.h"
+#include "Rules.h"
 #include <iostream>
 
 class Player {
 public:
 
    Player(std::string name);
-   Player(std::string name, int points);
+   Player() = default;
    ~Player();
    std::string getName();
    int getPoints();
    void addPoints(int points);
-
+   int countStorage(int row, Tile tile);
+   void setStorage(int row, LinkedList* toInsert);
+   void initialiseBoard();
+   void printStorageLine(int row);
+   void printMosaicLine(int row);
+   int calcScore(int row, int col);
+   void addToBroken(Tile tile);
+   void moveToMosaic(int row, Tile tile);
+   void clearStorageRow(int row);
+   Tile getTile(int row);
+   LinkedList* getBroken();
+   bool checkComplete(int row);
+   Tile mosaic[SIZE][SIZE]; // Momentarily placed in public for testing
 private:
 
     std::string name;
     int points;
+    Tile** storage = new Tile*[SIZE];
+    
+    LinkedList* broken;
 
 };
 
